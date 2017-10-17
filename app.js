d9af89cb425831778aaa3bfa27a02870ea2e21a5
maze <umazenauer@hotmail.com>
2017-10-17 19:50:33 +0200

    initial load root

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database')
+
+// Connect To Database
+const mongoDb = mongoose.connect(config.database, {
+    useMongoClient: true
+});
+
+mongoDb
+.then((db) => {
+    console.log('Connected to database '+config.database);
+})
+.catch((err) => {
+    console.log('Database error: '+err);
+});
+
+const app = express();
+
+const users = require('./routes/users');
+
+// Port Number
+const port = 3000;
+
+// CORS Middleware
+app.use(cors());
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+
+app.use('/users', users);
+
+// Index Route
+app.get('/', (req, res) => {
+    res.send('Invalide Endpoint');
+});
+
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+// Start Server
+app.listen(port, () => {
+    console.log('Server started on port '+port);
+});
